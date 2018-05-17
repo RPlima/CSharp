d6a3f9dd62b95f1c282ca9a80222376207a2198e
Raphael <raphaeltwins@hotmail.com>
2018-05-17 08:49:27 -0300

    Criando o método Salvar e a criação da list

---
+++ Aulas/Aula0705_console/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,25 @@
+﻿using Aula1705_Camadas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        //simulando tabela
+        public List<Atividade>ListaAtividades { get; set; }
+        //Salvar
+        public void Salvar(Atividade atividade)
+        {
+            ListaAtividades.Add(atividade);
+        }
+        //Listar
+
+        //Ler
+        //Editar
+        //Excluir
+    }
+}
